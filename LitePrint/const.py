f11cf68b111a72042b691f8959b553b81ca76a1e
lixianjun <xjli@mail.hnust.edu.cn>
2026-01-07 16:38:29 +0800

    更新了部分文件

---
+++ LitePrint/const.py
@@ -0,0 +1,45 @@
+dataset_lib = {
+    "CW":{'num_tabs':1,'maximum_load_time': 80,'name':'CW'},
+    "trafficsilver_bwr_CW":{'num_tabs':1,'maximum_load_time': 80,'name':'BWR'},
+    "trafficsilver_rb_CW":{'num_tabs':1,'maximum_load_time': 80,'name':'RB'},
+    "trafficsilver_bd_CW":{'num_tabs':1,'maximum_load_time': 80,'name':'BD'},
+    "wtfpad_CW":{'num_tabs':1,'maximum_load_time': 80,'name':'Pad'},
+    "front_CW":{'num_tabs':1,'maximum_load_time': 80,'name':'Front'},
+    "regulator_CW":{'num_tabs':1,'maximum_load_time': 80,'name':'Regula'},
+    "tamaraw_CW":{'num_tabs':1,'maximum_load_time': 80,'name':'Tamaraw'},
+
+
+
+
+    "Closed_2tab":{'num_tabs':2,'maximum_load_time': 120,'name':'CW_2tab'},
+    "Closed_3tab":{'num_tabs':3,'maximum_load_time': 120,'name':'CW_3tab'},
+    "Closed_4tab":{'num_tabs':4,'maximum_load_time': 120,'name':'CW_4tab'},
+    "Closed_5tab":{'num_tabs':5,'maximum_load_time': 120,'name':'CW_5tab'},
+    "wtfpad_Closed_2tab":{'num_tabs':2,'maximum_load_time': 120,'name':'CW_Pad'},
+    "front_Closed_2tab":{'num_tabs':2,'maximum_load_time': 120,'name':'CW_Fro'},
+    "regulator_Closed_2tab":{'num_tabs':2,'maximum_load_time': 120,'name':'CW_Reg'},
+    "Open_2tab":{'num_tabs':2,'maximum_load_time': 120,'name':'OW_2tab'},
+    "Open_3tab":{'num_tabs':3,'maximum_load_time': 120,'name':'OW_3tab'},
+    "Open_4tab":{'num_tabs':4,'maximum_load_time': 120,'name':'OW_4tab'},
+    "Open_5tab":{'num_tabs':5,'maximum_load_time': 120,'name':'OW_5tab'},
+    "wtfpad_Open_2tab":{'num_tabs':2,'maximum_load_time': 120,'name':'OW_Pad'},
+    "front_Open_2tab":{'num_tabs':2,'maximum_load_time': 120,'name':'OW_Fro'},
+    "regulator_Open_2tab":{'num_tabs':2,'maximum_load_time': 120,'name':'OW_Reg'},
+    # "":{'num_tabs':,'maximum_load_time': ,'name':''},
+}
+
+valid_note = {
+    "ExploreModel":
+        [
+            'baseline_same',
+            'baseline_optim',
+            'baseline_same_ablation_no_no',
+            'baseline_same_ablation_SWIFT_no',
+            'baseline_same_ablation_no_LINA',
+            'baseline_same_ablation_SWIFT_no',
+        ],
+    "other":
+        [
+            'baseline_same'
+        ]
+}
